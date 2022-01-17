569baee22f3e596ca1775591945eef75a05abf63
yaoyang <kiki@smarty.so>
2022-01-17 20:00:34 +0800

    清理项目不用的部分

--- project/tool/production/after_push.sh
+++ project/tool/production/after_push.sh
@@ -8,7 +8,3 @@ ln -fs $ROOT_DIR/project/config/production/nginx/recommend_tag_engine.conf /etc/
 /bin/bash $ROOT_DIR/project/tool/dep_build.sh link
 /usr/bin/php $ROOT_DIR/public/cli.php migrate:install
 /usr/bin/php $ROOT_DIR/public/cli.php migrate
-
-ln -fs $ROOT_DIR/project/config/production/supervisor/recommend_tag_engine_queue_worker.conf /etc/supervisor/conf.d/recommend_tag_engine_queue_worker.conf
-/usr/bin/supervisorctl update
-/usr/bin/supervisorctl restart recommend_tag_engine_queue_worker:*
